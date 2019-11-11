6711f38cac59f1a113d1b56d6cc9f707d4a988f1
DMB-220 <dmb2200@gmail.com>
2019-11-11 08:11:05 +0200

    as

--- resources/js/views/Pagrindinis.vue
+++ resources/js/views/Pagrindinis.vue
@@ -2,8 +2,8 @@
   <section class="section is-main-section">
       <card-component title="INFORMACIJA" icon="account-multiple">
         Duomenys atnaujinti<br>
-        Likučiai: 2019-11-08<br>
-        Pardavimai: 2019-10-08 --- 2019-11-08
+        Likučiai: 2019-11-11<br>
+        Pardavimai: 2019-10-11 --- 2019-11-11
 
       </card-component>     
     </section>
